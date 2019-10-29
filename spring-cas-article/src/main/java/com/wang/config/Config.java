fe018c78765e06d37d75acf841e118f3d9d67704
wanghe <2290370311@qq.com>
2019-10-29 18:18:46 +0800

    cas认证单点登录，主要是客户端的配置

---
+++ spring-cas-article/src/main/java/com/wang/config/Config.java
@@ -0,0 +1,89 @@
+package com.wang.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jasig.cas.client.authentication.AuthenticationFilter;
+import org.jasig.cas.client.session.SingleSignOutFilter;
+import org.jasig.cas.client.session.SingleSignOutHttpSessionListener;
+import org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+	
+	@Bean
+	public FilterRegistrationBean casFilterRegistration() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(casFilter());
+		Map<String, String>initParameters=new HashMap<String, String>();
+		initParameters.put("casServerLoginUrl", "http://127.0.0.1:8080/cas");
+		initParameters.put("serverName", "http://127.0.0.1:8002");
+		registration.setInitParameters(initParameters);
+		registration.addUrlPatterns("/*");
+		registration.setOrder(1);
+		return registration;
+	}
+	
+	/**
+	 *
+	 * 该过滤器负责用户的认证工作，必须启用它
+	 * @return
+	 */
+	@Bean
+	public AuthenticationFilter casFilter() {
+		AuthenticationFilter authenticationFilter=new AuthenticationFilter();
+		return authenticationFilter; 
+	}
+	
+	/**
+	 * 该过滤器负责对Ticket的校验工作，必须启用它 
+	 * @return
+	 */
+	@Bean
+	public Cas20ProxyReceivingTicketValidationFilter casValidateFilter() {
+		Cas20ProxyReceivingTicketValidationFilter casValidateFilter=new Cas20ProxyReceivingTicketValidationFilter();
+		return  casValidateFilter;
+	}
+	
+	
+	@Bean
+	public FilterRegistrationBean cas20ProxyReceivingTicketValidationFilter() { 
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(casValidateFilter());
+		Map<String, String>initParameters=new HashMap<String, String>();
+		initParameters.put("casServerUrlPrefix", "http://127.0.0.1:8080/cas");
+		initParameters.put("serverName", "http://127.0.0.1:8002");
+		registration.setInitParameters(initParameters);
+		registration.addUrlPatterns("/*");
+		registration.setOrder(2);
+		return registration;
+	}
+	
+	
+	/**
+	 * 与下面的监听器配合，实现单点退出
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean singleoutFilter() {
+		FilterRegistrationBean signOutFilter=new FilterRegistrationBean();
+		signOutFilter.setFilter(new SingleSignOutFilter());
+		signOutFilter.addUrlPatterns("/*");
+		signOutFilter.setOrder(3);
+		return signOutFilter;
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Bean
+	 public ServletListenerRegistrationBean servletListenerRegistrationBean(){
+		ServletListenerRegistrationBean servletListenerRegistrationBean = new ServletListenerRegistrationBean();
+		servletListenerRegistrationBean.setListener(new SingleSignOutHttpSessionListener());
+		return servletListenerRegistrationBean;
+	 }
+	
+	
+}
